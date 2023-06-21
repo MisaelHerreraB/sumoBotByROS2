ce399a66d447471de6dbc4c3045ad34e9e238fec
vboxuser <vboxuser@ROS2.myguest.virtualbox.org>
2023-06-20 22:00:46 -0500

    push Follower finished

--- pushFollowerCentralServer.py
+++ pushFollowerCentralServer.py
@@ -2,10 +2,28 @@ from time import sleep
 import rclpy
 from std_msgs.msg import String
 
-
 def cb(msg):
-   print(f'I heard:"{msg.data}"')
-   msg.data = msg.data + f'+push'
+   aux = msg.data
+   val = f'izF'   
+   print(f'Publishing:"{aux}"')
+   print(f'Publishing:"{aux[3]}"')
+   if aux[0]=="0" and aux[1]=="1" and aux[2]=="1" and aux[3]=="0":
+      val = f'adelante'
+   if aux[0]=="0" and aux[1]=="0" and aux[2]=="0" and aux[3]=="0":
+      val = f'izF'
+   
+   if aux[0]=="0" and aux[1]=="0" and aux[2]=="0" and aux[3]=="1":
+      val = f'deF'
+   if aux[0]=="1" and aux[1]=="0" and aux[2]=="0" and aux[3]=="0":
+      val = f'izF'
+   if aux[0]=="0" and aux[1]=="0" and aux[2]=="1" and aux[3]=="0":
+      val = f'deS'
+   if aux[0]=="0" and aux[1]=="1" and aux[2]=="0" and aux[3]=="0":
+      val = f'izS'
+   
+   if aux[6]=="1" or aux[5]=="1":
+      val = f'back'
+   msg.data = val
    print(f'Publishing:"{msg.data}"')
    pub.publish(msg)
    
